7e3e75214be6c0de0104205f34d93804796c1fcb
Chris Kim <chriskim06@gmail.com>
2016-01-27 16:03:55 -0800

    Fixing issue with one of the scripts

--- all-branches-github.js
+++ all-branches-github.js
@@ -4,7 +4,7 @@
 // @description Changes the link so that clicking branches takes you to the all branches page
 // @include     https://github.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-// @version     1.4.6
+// @version     1.4.7
 // @grant       none
 // @locale      en
 // ==/UserScript==
@@ -16,9 +16,11 @@ $(function() {
   function allBranches() {
     if ($('.repository-content').length) {
       var link = $('.repository-content').find('ul.numbers-summary').find('li:nth-child(2) > a');
-      var href = link.attr('href');
-      if (!href.endsWith('/all')) {
-        link.attr('href', href + '/all');
+      if (link.length) {
+        var href = link.attr('href');
+        if (href.length && !href.endsWith('/all')) {
+          link.attr('href', href + '/all');
+        }
       }
     }
   }
