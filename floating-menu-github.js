037585b63faa72ebfa7f4d83d6259fd74d4cdec7
Chris Kim <chriskim06@gmail.com>
2016-01-29 23:28:15 -0800

    Fixed some mistakes in newest script

--- floating-menu-github.js
+++ floating-menu-github.js
@@ -3,7 +3,7 @@
 // @namespace   chriskim06
 // @description Makes the menu bar in GitHub stay at the top of the page when scrolling
 // @include     https://github.com/*
-// @version     1.0.0
+// @version     1.0.1
 // @grant       none
 // @locale      en
 // ==/UserScript==
@@ -12,12 +12,12 @@
   var menu = document.getElementsByClassName('header header-logged-in true');
   if (menu.length) {
     menu[0].style.position = 'fixed';
-    menu[0].style.z-index = '10000';
+    menu[0].style.zIndex = '10000';
     menu[0].style.width = '100%';
     var content = document.getElementsByClassName('main-content');
     if (content.length) {
-      menu[0].style.position = 'relative';
-      menu[0].style.top = '50px';
+      content[0].style.position = 'relative';
+      content[0].style.top = '50px';
     }
   }
 })();
