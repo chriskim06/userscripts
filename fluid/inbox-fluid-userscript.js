b3130c416d9af1866364a9a056335eeda423fd40
Chris Kim <chriskim06@gmail.com>
2016-02-28 09:29:20 -0800

    Switched from webkitNotifications to the Notifications API and simplified some of the logic

--- fluid/inbox-fluid-userscript.js
+++ fluid/inbox-fluid-userscript.js
@@ -1,5 +1,5 @@
 /**
- * This is a FluidApp userscript for Google Inbox for Mac. 
+ * This is a FluidApp userscript for Google Inbox for Mac.
  * It displays an unread email count on the dock icon and creates notifications when new emails come in.
  *
  * 1) You need the paid version of FluidApp (http://fluidapp.com/)
@@ -7,59 +7,52 @@
  * 3) Set the user-agent to Chrome
  * 4) Go to window -> userscript and add a new userscript called 'Inbox' that matches the pattern "*inbox.google.com*".
  * 5) Paste the contents of this document into the script area below that.
- * 6) Refresh your FluidApp. (CMD + R)
+ * 6) Refresh your FluidApp.
  */
 
 window.fluid.dockBadge = '';
-setTimeout(updateBadge, 5000);
-setInterval(updateBadge, 5000);
+setTimeout(updateBadge, 1000);
+setInterval(updateBadge, 3000);
 
+// Looks for and counts unread emails
 function updateBadge() {
   var prevCount = window.fluid.dockBadge;
-  // Inbox title is shown/hidden depending on the selected folder (done, sent, etc)
-  var inboxTabSpan = document.getElementsByClassName('bl')[0];
-  if (inboxTabSpan.getAttribute('title') != 'Inbox' && prevCount !== '') {
-    // Inbox is hidden.
-    window.fluid.dockBadge = '?';
-  } else {
-    // Check for Inbox Zero
-    var inboxZero = document.getElementsByClassName('j');
-    if (inboxZero.length >= 1) {
-      window.fluid.dockBadge = '';
-    } else {
-      // Inbox is visible
+  var navTitle = document.querySelectorAll('[jsaction="global.navigate_and_refresh"]')[0].textContent.trim();
+  if (navTitle === 'Inbox') {
+    var emptyImage = document.getElementsByClassName('j');
+    if (emptyImage.length === 0) {
       var unreadEmails = document.getElementsByClassName('ss');
       var count = unreadEmails.length;
-      if (count >= 1) {
-        window.fluid.dockBadge = count;
-        if (window.Notification && (prevCount === '?' || count > prevCount)) {
-          // Create notification if there are new emails or unread emails when returning to the inbox tab
-          buildAndShowNotification(count);
-        }
-      } else {
-        // There are no unread emails in the inbox
-        window.fluid.dockBadge = '';
+      window.fluid.dockBadge = (count === 0) ? '' : count;
+      if (window.Notification && count > prevCount) {
+        buildNotification(count);
       }
     }
   }
 }
 
-function buildAndShowNotification(count) {
-  var permission = Notification.permission;
-  if (permission === 'default') {
-    Notification.requestPermission(function() {
-      notify();
+// Checks notification permission
+function buildNotification(count) {
+  if (Notification.permission === 'granted') {
+    showNotification('Inbox', count);
+  } else {
+    Notification.requestPermission().then(function(result) {
+      if (result === 'denied') {
+        console.log('Permission denied.');
+      } else if (result === 'default') {
+        console.log('The permission request was dismissed.');
+      } else if (result === 'granted') {
+        showNotification('Inbox', count);
+      }
     });
-  } else if (permission === 'granted') {
-    var content = count + ' unread message';
-    if (count > 1) {
-      content += 's';
-    }
-    var notification = window.webkitNotifications.createNotification(null, 'Inbox', content);
-    try {
-      notification.show();
-    } catch (e) {
-      console.log('Error showing notification');
-    }
   }
 }
+
+// Create and show the notification
+function showNotification(title, count) {
+  var options = {
+    body: (count === 1) ? '1 unread message' : count + ' unread messages'
+  }
+  var n = new Notification(title, options);
+}
+
