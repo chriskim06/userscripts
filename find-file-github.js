2f39ff76779bed3ce12105faa9fb9eb41b32c39b
Chris Kim <chriskim06@gmail.com>
2016-02-28 10:29:26 -0800

    Doing without jquery and simplified by using regex to get the branch as well

--- find-file-github.js
+++ find-file-github.js
@@ -3,24 +3,18 @@
 // @namespace   chriskim06
 // @description Adds a keyboard shortcut to use GitHub's find file feature
 // @include     https://github.com/*/*
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-// @version     1.0
+// @version     1.1
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
-this.$ = this.jQuery = jQuery.noConflict(true);
-
-$(function() {
-
-  $(document).on('keydown', function(e) {
-    if (e.which === 80 && e.shiftKey && e.target.nodeName !== 'INPUT') {
-      var url = window.location.href.match(/https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+/);
-      var branch = $('div.select-menu.js-menu-container.js-select-menu.left').find('span.js-select-button.css-truncate-target');
-      if (branch.length) {
-        window.location.href = url + '/find/' + branch.html();
-      }
+var page = document.querySelector('html');
+page.addEventListener('keydown', function(e) {
+  if (e.which === 80 && e.shiftKey && e.target.nodeName !== 'INPUT') {
+    var url = window.location.href.match(/(https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+)(?:\/tree\/)?([A-Za-z0-9_-]+)?/);
+    if (url !== null) {
+      var branch = (y[2]) ? y[2] : 'master';
+      window.location.href = url[1] + '/find/' + branch;
     }
-  });
-
+  }
 });
