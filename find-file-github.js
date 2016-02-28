d4adb65d652d461280695b29726647785f7710f8
Chris Kim <ckim@fastspring.com>
2016-02-28 12:17:51 -0800

    Switched back to using a selector to figure out what branch you're on

--- find-file-github.js
+++ find-file-github.js
@@ -3,17 +3,22 @@
 // @namespace   chriskim06
 // @description Adds a keyboard shortcut to use GitHub's find file feature
 // @include     https://github.com/*/*
-// @version     1.3
+// @version     1.4
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
 document.querySelector('html').addEventListener('keydown', function(e) {
   if (e.shiftKey && e.which === 80 && e.target.nodeName !== 'INPUT') {
-    var url = window.location.href.match(/(https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+)(?:\/tree\/)?([A-Za-z0-9_-]+)?/);
-    if (url !== null) {
-      var branch = (url[2]) ? url[2] : 'master';
-      window.location.href = url[1] + '/find/' + branch;
+    var url = window.location.href.match(/(https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+)/);
+    if (url) {
+      var branchMenu = document.getElementsByClassName('btn btn-sm select-menu-button js-menu-target css-truncate')[0];
+      if (branchMenu) {
+        var branch = branchMenu.getElementsByClassName('js-select-button css-truncate-target')[0];
+        if (branch) {
+          window.location.href = url[1] + '/find/' + branch.innerHTML;
+        }
+      }
     }
   }
 });
