e84e1880f787203c6d74085116b077696b15f543
Chris Kim <chriskim06@gmail.com>
2016-02-28 10:40:55 -0800

    Adding jquery back in because its way easier

--- find-file-github.js
+++ find-file-github.js
@@ -3,18 +3,24 @@
 // @namespace   chriskim06
 // @description Adds a keyboard shortcut to use GitHub's find file feature
 // @include     https://github.com/*/*
-// @version     1.1
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @version     1.2
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
-var page = document.querySelector('html');
-page.addEventListener('keydown', function(e) {
-  if (e.which === 80 && e.shiftKey && e.target.nodeName !== 'INPUT') {
-    var url = window.location.href.match(/(https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+)(?:\/tree\/)?([A-Za-z0-9_-]+)?/);
-    if (url !== null) {
-      var branch = (y[2]) ? y[2] : 'master';
-      window.location.href = url[1] + '/find/' + branch;
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+$(function() {
+
+  $(document).on('keydown', function(e) {
+    if (e.which === 80 && e.shiftKey && e.target.nodeName !== 'INPUT') {
+      var url = window.location.href.match(/(https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+)(?:\/tree\/)?([A-Za-z0-9_-]+)?/);
+      if (url !== null) {
+        var branch = (y[2]) ? y[2] : 'master';
+        window.location.href = url[1] + '/find/' + branch;
+      }
     }
-  }
+  });
+
 });
