31f77d2ad75bc348e0ce55ddb6a5bbaa3228891f
Chris Kim <chriskim06@gmail.com>
2016-02-28 10:08:38 -0800

    Using 'P' as the shortcut now and checking that the target is not an input

--- find-file-github.js
+++ find-file-github.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Find File Shortcut (GitHub)
 // @namespace   chriskim06
-// @description Use ctrl-f to use GitHub's find file feature
+// @description Adds a keyboard shortcut to use GitHub's find file feature
 // @include     https://github.com/*/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @version     1.0
@@ -14,7 +14,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 $(function() {
 
   $(document).on('keydown', function(e) {
-    if (e.which === 70 && e.ctrlKey) {
+    if (e.which === 80 && e.shiftKey && e.target.nodeName !== 'INPUT') {
       var url = window.location.href.match(/https:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+/);
       var branch = $('div.select-menu.js-menu-container.js-select-menu.left').find('span.js-select-button.css-truncate-target');
       if (branch.length) {
