692f7569c7ab3578b30b4908ae72aa1e72a853c7
Chris Kim <chriskim06@gmail.com>
2016-03-29 19:13:05 -0700

    Removing jquery from a couple userscripts

--- default-behavior-greasyfork.js
+++ default-behavior-greasyfork.js
@@ -4,27 +4,26 @@
 // @description Add/edit scripts with syntax highlighting enabled by default and hide share stuff
 // @include     /https://greasyfork.org/en/scripts/\w+(-|/).*/
 // @include     https://greasyfork.org/en/script_versions/new
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-// @version     1.2.3
+// @version     1.2.4
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
-this.$ = this.jQuery = jQuery.noConflict(true);
+(function() {
 
-$(function() {
-
-  if ($('#script_version_code').length) {
-    $('#script_version_code').css('height', '560px');
+  var textarea = document.getElementById('script_version_code');
+  if (textarea.length) {
+    textarea.style.height = '560px';
   }
 
-  var checkbox = $('#enable-source-editor-code');
-  if (checkbox.length && !checkbox.is(':checked')) {
-    checkbox.click();
+  var checkbox = document.getElementById('enable-source-editor-code');
+  if (checkbox.length && !checkbox.checked) {
+    checkbox.checked = true;
   }
   
-  if ($('div#share').length) {
-    $('div#share').css('display', 'none');
+  var share = document.getElementById('share');
+  if (share.length) {
+    share.style.display = 'none';
   }
 
-});
+})();
