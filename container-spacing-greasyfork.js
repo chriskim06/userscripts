eb0ed2380884247120bcd52806964556059fa8a8
Chris Kim <chriskim06@gmail.com>
2016-07-09 16:59:14 -0700

    Adding margin to deleted scripts section if it exists

--- container-spacing-greasyfork.js
+++ container-spacing-greasyfork.js
@@ -4,26 +4,29 @@
 // @description Adds a small margin to the bottom of the code container and author description in Greasy Fork
 // @include     https://greasyfork.org/en/scripts/*
 // @include     https://greasyfork.org/en/users/*
-// @version     1.2.3
+// @version     1.2.4
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
 (function() {
-
-  var elem = document.getElementById('code-container');
-  if (elem !== null) {
-    elem.setAttribute('style', 'margin-bottom: 60px;');
+  var el = document.getElementById('code-container');
+  if (el !== null) {
+    el.style.marginBottom = '60px';
   }
   
-  var elem = document.getElementById('additional-info');
-  if (elem !== null) {
-    elem.setAttribute('style', 'margin-bottom: 60px;');
+  el = document.getElementById('additional-info');
+  if (el !== null) {
+    el.style.marginBottom = '60px';
   }
   
-  var elem = document.getElementById('user-script-list');
-  if (elem !== null) {
-    elem.setAttribute('style', 'margin-bottom: 60px;');
+  el = document.getElementById('user-deleted-script-list');
+  if (el !== null) {
+    el.style.marginBottom = '60px';
+  } else {
+    el = document.getElementById('user-script-list');
+    if (el !== null) {
+      el.style.marginBottom = '60px';
+    }
   }
-
 })();
