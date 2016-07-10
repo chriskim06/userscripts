b446e276b54216fff02f340f186e58a2e3951121
Chris Kim <chriskim06@gmail.com>
2016-07-09 18:42:47 -0700

    Creating all elements in separate function

--- add-links-greasyfork.js
+++ add-links-greasyfork.js
@@ -3,47 +3,44 @@
 // @namespace   chriskim06
 // @description Add links to navigate to the update tab and links to install scripts
 // @include     https://greasyfork.org/en/users/*
-// @version     1.3.1
+// @version     1.3.2
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
 (function() {
 
-  function createSpan(link, text) {
-    var el = document.createElement('span');
-    el.className = 'name-description-separator';
+  function createNewElement(link, tag, text, href) {
+    var el = document.createElement(tag);
+    if (tag === 'a') {
+      el.href = href;
+    } else {
+      el.className = 'name-description-separator';
+    }
     el.innerHTML = text;
     link.parentNode.insertBefore(el, link.nextElementSibling);
   }
 
-  var currentUser = document.querySelector('#nav-user-info > .user-profile-link > a').innerHTML;
-  var loggedIn = (currentUser === document.querySelector('title').innerHTML);
-  var scripts = document.getElementById('user-script-list');
-  if (scripts !== null) {
+  if (document.querySelector('#user-script-list') !== null) {
+    var currentUser = document.querySelector('#nav-user-info > .user-profile-link > a');
+    var loggedIn = (currentUser !== null && currentUser.innerHTML === document.title);
     var items = document.querySelectorAll('#user-script-list > li');
     for (var i = 0; i < items.length; i++) {
       var link = items[i].querySelector('a');
-      link.href = link.href + '/code';
       if (loggedIn) {
-        var el = document.createElement('a');
-        el.href = '/en/scripts/' + items[i].dataset.scriptId + '/versions/new';
-        el.innerHTML = 'Edit';
-        link.parentNode.insertBefore(el, link.nextElementSibling);
-        createSpan(link, ' - ');
-        el = document.createElement('a');
-        el.href = link.href + '/delete';
-        el.innerHTML = 'Delete';
-        link.parentNode.insertBefore(el, link.nextElementSibling);
-        createSpan(link, '/');
+        createNewElement(link, 'a', 'Edit', '/en/scripts/' + items[i].dataset.scriptId + '/versions/new');
+        createNewElement(link, 'span', ' - ', null);
+        createNewElement(link, 'a', 'Delete', link.href + '/delete');
+        createNewElement(link, 'span', '/', null);
       }
-      var install = document.createElement('a');
-      install.href = link.href + '/code/' + link.innerHTML.replace(/ /g, '%20') + '.user.js';
-      install.innerHTML = 'Install';
-      link.parentNode.insertBefore(install, link.nextElementSibling);
-      createSpan(link, ' - ');
+      createNewElement(link, 'a', 'Install', link.href + '/code/' + link.innerHTML.replace(/ /g, '%20') + '.user.js');
+      createNewElement(link, 'span', ' - ', null);
+      link.href = link.href + '/code';
     }
-    scripts.previousElementSibling.querySelector('h3').innerHTML = 'Scripts (' + items.length + ')';
+    var scripts = document.querySelector('#user-script-list').parentElement.querySelector('header > h3');
+    if (scripts !== null) {
+      scripts.innerHTML = 'Scripts (' + items.length + ')';
+    }
   }
 
 })();
