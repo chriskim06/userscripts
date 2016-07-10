3cf3478f105cda1cd73935ffc71dbe109dd8ff96
Chris Kim <chriskim06@gmail.com>
2016-07-09 17:49:47 -0700

    Removed jQuery and fixed mistake

--- add-links-greasyfork.js
+++ add-links-greasyfork.js
@@ -3,33 +3,43 @@
 // @namespace   chriskim06
 // @description Add links to navigate to the update tab and links to install scripts
 // @include     https://greasyfork.org/en/users/*
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-// @version     1.2.9
+// @version     1.3.0
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
-this.$ = this.jQuery = jQuery.noConflict(true);
+(function() {
 
-$(function() {
-
-  if ($('#user-script-list').length) {
-    var scripts = $('#user-script-list');
-    var count = scripts.children('li').length;
-    scripts.children('li').each(function() {
-      var item = $(this);
-      var link = item.find('a:first');
-      var href = link.attr('href');
-      var file = link.html().replace(/ /g, '%20') + '.user.js';
-      link.attr('href', href + '/code');
-      link.after('<a href="/en/scripts/' + item.attr('data-script-id') + '/versions/new">Edit</a>');
-      link.after('<span class="name-description-separator"> - </span>');
-      link.after('<a href="' + href + '/delete">Delete</a>');
-      link.after('<span class="name-description-separator">/</span>');
-      link.after('<a href="' + href + '/code/' + file + '">Install</a>');
-      link.after('<span class="name-description-separator"> - </span>');
-    });
-    scripts.prev().children().first().append(' (' + count + ')');
+  function createSpan(link, text) {
+    var el = document.createElement('span');
+    el.className = 'name-description-separator';
+    el.innerHTML = text;
+    link.parentNode.insertBefore(el, link.nextElementSibling);
   }
 
-});
+  var scripts = document.getElementById('user-script-list');
+  if (scripts !== null) {
+    var items = document.querySelectorAll('#user-script-list > li');
+    for (var i = 0; i < items.length; i++) {
+      var link = items[i].querySelector('a');
+      var el = document.createElement('a');
+      el.href = '/en/scripts/' + items[i].dataset.scriptId + '/versions/new';
+      el.innerHTML = 'Edit';
+      link.parentNode.insertBefore(el, link.nextElementSibling);
+      createSpan(link, ' - ');
+      el = document.createElement('a');
+      el.href = link.href + '/delete';
+      el.innerHTML = 'Delete';
+      link.parentNode.insertBefore(el, link.nextElementSibling);
+      createSpan(link, '/');
+      el = document.createElement('a');
+      el.href = link.href + '/code/' + link.innerHTML.replace(/ /g, '%20') + '.user.js';
+      el.innerHTML = 'Install';
+      link.parentNode.insertBefore(el, link.nextElementSibling);
+      createSpan(link, ' - ');
+      link.href = link.href + '/code';
+    }
+    scripts.previousElementSibling.querySelector('h3').innerHTML = 'Scripts (' + items.length + ')';
+  }
+
+})();
--- default-behavior-greasyfork.js
+++ default-behavior-greasyfork.js
@@ -4,7 +4,7 @@
 // @description Add/edit scripts with syntax highlighting enabled by default and hide share stuff
 // @include     /https://greasyfork.org/en/scripts/\w+(-|/).*/
 // @include     https://greasyfork.org/en/script_versions/new
-// @version     1.2.4
+// @version     1.2.5
 // @grant       none
 // @locale      en
 // ==/UserScript==
@@ -12,17 +12,17 @@
 (function() {
 
   var textarea = document.getElementById('script_version_code');
-  if (textarea.length) {
+  if (textarea !== null) {
     textarea.style.height = '560px';
   }
 
   var checkbox = document.getElementById('enable-source-editor-code');
-  if (checkbox.length && !checkbox.checked) {
+  if (checkbox !== null && !checkbox.checked) {
     checkbox.checked = true;
   }
   
   var share = document.getElementById('share');
-  if (share.length) {
+  if (share !== null) {
     share.style.display = 'none';
   }
 
