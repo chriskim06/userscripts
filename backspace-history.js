32aa824da96a67f33b466a8139ae8da7a32018e7
Chris Kim <ckim@fastspring.com>
2016-07-09 22:45:50 -0700

    Fixing backspace script

--- backspace-history.js
+++ backspace-history.js
@@ -3,13 +3,15 @@
 // @namespace   chriskim06
 // @description Makes the backspace key go back in history instead of modifying the search string
 // @include     https://www.google.com/*
-// @version     1.6
+// @version     1.7
 // @grant       none
 // @locale      en
 // ==/UserScript==
 
 document.querySelector('html').addEventListener('keydown', function(e) {
   if (!e.shiftKey && e.which === 8 && e.target.nodeName !== 'INPUT') {
+    e.preventDefault();
+    e.stopImmediatePropagation();
     window.history.back();
     return false;
   }
